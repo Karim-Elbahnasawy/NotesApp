d95c7a4f7be49df8a7da7d372e4ce96941875551
abc <abc@example.com>
2024-06-27 16:04:06 +0300

    Create Notes App

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
