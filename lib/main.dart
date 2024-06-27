ebde80f62bc4933628f8660d43ed937bad7d300c
abc <abc@example.com>
2024-06-27 16:32:04 +0300

    Create Notes View and change theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
     );
   }
 }
