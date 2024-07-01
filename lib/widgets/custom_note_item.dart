66b706c4b9cc4c498b99da78c7f7ace083750c0f
abc <abc@example.com>
2024-07-01 16:28:24 +0300

    create edit view body

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../views/edit_note_view.dart';
+
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
@@ -7,56 +9,66 @@ class CustomNoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4.0),
-      child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCCB0),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Padding(
-                padding: EdgeInsets.only(bottom: 16),
-                child: Text(
-                  'Flutter Tips',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 26,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return EditNoteView();
+            }),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCCB0),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Padding(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    'Flutter Tips',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 26,
+                    ),
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    'Build Your Career With Karim',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.6),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  color: Colors.black,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.delete,
+                    size: 25,
                   ),
                 ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(bottom: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  'Build Your Career With Karim',
+                  'May 20, 2022',
                   style: TextStyle(
                     color: Colors.black.withOpacity(0.6),
-                    fontSize: 18,
+                    fontSize: 16,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                color: Colors.black,
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  size: 25,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'May 20, 2022',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.6),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
